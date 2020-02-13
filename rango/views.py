505b31e4fcabe33dcbd4faf76861a61d1e65a300
Andrew Yao <2389728y@student.gla.ac.uk>
2020-02-13 20:46:49 +0000

    Chapter 10 Passed Tests

--- rango/views.py
+++ rango/views.py
@@ -15,12 +15,12 @@ def get_server_side_cookie(request, cookie, default_val=None):
 
 def visitor_cookie_handler(request):
     visits = int(get_server_side_cookie(request, 'visits', '1'))
-    last_visit_cookie = get_server_side_cookie(request, 'last_vist', str(datetime.now()))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
     last_vist_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
 
-    if (datetime.now() - last_vist_time).days > 0:
+    if (datetime.now() - last_vist_time).seconds > 0:
         visits = visits + 1
-        request.session['last_vist'] = str(datetime.now())
+        request.session['last_visit'] = str(datetime.now())
     else:
         request.session['last_visit'] = last_visit_cookie
     request.session['visits'] = visits
